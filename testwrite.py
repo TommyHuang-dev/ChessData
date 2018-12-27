80f2fb9bbe5c7aeaca115f3dbbcc05e901c4af6c
Tommy Huang <tommyhuang999@gmail.com>
2018-12-27 16:31:57 -0500

    now ignores commentary!!

--- testwrite.py
+++ testwrite.py
@@ -8,36 +8,47 @@ import csv
 # formatting (dictionary): {'year': {'opening': {'white wins': num0, 'draws': num1, 'black wins': num2}}}
 
 finalDict = {}
+IN_NAME = "Sample Database 2.txt"
+OUT_NAME = "chessParseData.csv"
 
 # read and parse
-with open("Sample Database.txt", "r", errors="replace") as inFile:
+with open(IN_NAME, "r", errors="replace") as inFile:
     # inFile.readline()
     for line in inFile:
         # ECO code
         openCode = line.rstrip()[-5:]
+
         # year
         line = inFile.readline()
         year = line.rstrip()[-4:]
+
+        # read through all stuffs
+        while len(line) > 1:
+            line = inFile.readline()
+
         # result (1-0, ½-½, 0-1)
-        line = inFile.readline()
-        line = inFile.readline()
+        lineNew = inFile.readline()
+        while len(lineNew) > 1:
+            lineOld = lineNew
+            lineNew = inFile.readline()
+        line = lineOld
+
         result = line.rstrip()[-3:]
-        # convert (-1 black wins, 0 draw, 1 white wins)
+        # convert result (-1 black wins, 0 draw, 1 white wins)
         if result == '1-0':
             result = '1'
         elif result == '0-1':
             result = '-1'
-        elif result == '-Â½':
+        elif result[-1] == "½":
             result = '0'
         else:
-            print("SOMETHING NOT FORMATTED A fdkabewalfabef ")
-            print(line)
+            print("dis line: " + line)
 
         print(openCode, year, result)
-        line = inFile.readline()
-3
+
+
 # write to chessParsedData.csv
-with open('chessParsedData.csv', 'w', newline='') as outFile:
+with open(OUT_NAME, 'w', newline='') as outFile:
     writer = csv.writer(outFile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
